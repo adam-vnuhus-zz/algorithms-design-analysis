9d589755dcf0a54771f849bd9f2fe2939c99b6fe
Dam Tuan Anh <tuananh1108.97@gmail.com>
2019-04-04 22:56:32 +0700

    LIS update

--- src/BT6/LongestIncreasingSubsequence.java
+++ src/BT6/LongestIncreasingSubsequence.java
@@ -1,65 +1,55 @@
 package BT6;
 
 public class LongestIncreasingSubsequence {
-
-    static int a[] = {2, 5, 4, 6, 3, 8, 9, 7};
-    static int L[] = new int[a.length + 1];
-
-    static int max() {
-        int max = 0;
-
-        for (int i = 0; i < a.length; i++) {
-            L[i] = 1;
-        }
-
-        for (int i = 1; i < a.length; i++) {
+    public void findSubsequence(int[] arrA) {
+        int[] LIS = new int[arrA.length];
+        for (int i = 0; i < arrA.length; i++) {
+            int max = -1;
             for (int j = 0; j < i; j++) {
-                if (a[i] > a[j] && L[i] < L[j] + 1)
-                    L[i] = L[j] + 1;
-            }
-        }
-
-        for (int i = 0; i < a.length; i++) {
-            if (max < L[i])
-                max = L[i];
-        }
-
-        return max;
-    }
-
-    static void truyVet() {
-        //Bang phuong an
-        L[0] = 0;
-        for (int i = 1; i < a.length + 1; i++) {
-            L[i] = 1;
-            for (int j = 1; j < i + 1; j++) {
-                if (a[j - 1] < a[i - 1] && j < i && L[i] < L[j] + 1) {
-                    L[i] = L[j] + 1;
+// check if previous elements > current element
+                if (arrA[i] > arrA[j]) {
+// update the max from the previous entries
+                    if (max == -1 || max < LIS[j] + 1) {
+                        max = 1 + LIS[j];
+                    }
                 }
             }
+            if (max == -1) {
+// means none of the previous element has smaller than arrA[i]
+                max = 1;
+            }
+            LIS[i] = max;
         }
-        for (int i = 0; i < a.length + 1; i++) {
-            System.out.print(L[i] + "  ");
-        }
-        System.out.println();
-
-        //Day con tang dai nhat
-        int Max = max();
-        int result[] = new int[a.length];
-        for (int i = a.length; i >= 0; i--) {
-            if (L[i] == Max) {
-                result[i] = a[i];
-                Max--;
+// find the max in the LIS[]
+        int result = -1;
+        int index = -1;
+        for (int i = 0; i < LIS.length; i++) {
+            if (result < LIS[i]) {
+                result = LIS[i];
+                index = i;
             }
         }
-        for (int x : result) {
-            System.out.print("  " + x);
+// Print the result
+// Start moving backwards from the end and
+        String path = arrA[index] + " ";
+        int res = result - 1;
+        for (int i = index - 1; i >= 0; i--) {
+            if (LIS[i] == res) {
+                path = arrA[i] + " " + path;
+                res--;
+            }
         }
+        System.out.println("Longest Increasing subsequence: " + result);
+        System.out.println("Actual Elements: " + path);
     }
 
     public static void main(String[] args) {
-        System.out.println(max());
-        truyVet();
+//        int[] A = {1, 12, 7, 0, 23, 11, 52, 31, 61, 69, 70, 2};
+        int[] A = {2, 5, 4, 6, 3, 8, 9, 7};
+        LongestIncreasingSubsequence i = new LongestIncreasingSubsequence();
+        i.findSubsequence(A);
+        int[] B = {3, 4, 5, 6};
+        String o = B[0] +" "+ B[1];
+        System.out.println(o);
     }
-
 }
