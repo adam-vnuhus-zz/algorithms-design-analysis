37b5e2228347fb4e6565024feced0768ae623ddd
anhtdam <tuananh1108.97@gmail.com>
2019-04-14 20:06:46 +0700

    update knapsack problem

--- src/tlgk_branch_and_bound/KnapsackProblem.java
+++ src/tlgk_branch_and_bound/KnapsackProblem.java
@@ -1,6 +1,104 @@
 package tlgk_branch_and_bound;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class KnapsackProblem {
 
+    static int n; //so luong vat
+    static int m;   //can nang cua balo
+    static int[] value = new int[100];
+    static int[] weight = new int[100];
+    static int[] x = new int[100]; //luu nhung do vat duoc chon
+    static int[] y = new int[100];  //luu phuong an toi uu 0 & 1
 
+    static int vmax; //gia tri lon nhat
+    static int vsel;    //gia tri da duoc chon
+    static int wrem;    //trong luong con lai
+
+    static float b;
+
+    static void init() {
+        try {
+            Scanner scanner = new Scanner(new File("C:\\Users\\ANHTDAM\\IdeaProjects\\algorithms-design-analysis\\src\\input.txt"));
+            while (scanner.hasNext()){
+                n = scanner.nextInt();
+                m = scanner.nextInt();
+                for (int i = 1; i <= n; i++) {
+                    value[i] = scanner.nextInt();
+                }
+                for (int i = 1; i <= n; i++) {
+                    weight[i] = scanner.nextInt();
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        value[n + 1] = 0;
+        weight[n + 1] = 1;
+        vmax = 0;
+        wrem = m;
+        vsel = 0;
+        b = m * value[1] / (float) weight[1];
+
+    }
+
+    static void attempt(int i) {
+        for (int j = 0; j <= 1; j++) {
+            if (j == 1) {
+                b = vsel + value[i] + wrem * value[i + 1] / (float) weight[i + 1];
+                if (b >= vmax) {
+                    x[i] = j;
+                    vsel = vsel + value[i];
+                    wrem = wrem - weight[i];
+                }
+            }
+            if (i == n || wrem <= 0) {
+                ghiNhan();
+            } else {
+                attempt(i + 1);
+            }
+            if (j == 1) {
+                b = vsel + value[i] + wrem * value[i + 1] / (float) weight[i + 1];
+                if (b >= vmax) {
+                    x[i] = j;
+                    vsel = vsel - value[i];
+                    wrem = wrem + weight[i];
+                }
+            }
+        }
+    }
+
+    static void ghiNhan() {
+        if (vmax <= vsel && wrem >= 0) {
+            vmax = vsel;
+
+            for (int i = 1; i <= n; i++) {
+                y[i] = x[i];
+            }
+        }
+    }
+
+    static void outPut() {
+        int i;
+        System.out.println("Gia tri toi uu: \n" + vmax);
+        System.out.println("Chon cac do vat sau: ");
+        for (i = 1; i <= n; i++) {
+            if (y[i] != 0) {
+                System.out.print(i + "\t");
+            }
+        }
+//        for (i = 1; i <= n; i++) {
+//            if (y[i] != 0) {
+//                System.out.print(y[i] + "\t");
+//            }
+//        }
+    }
+
+    public static void main(String[] args) {
+        init();
+        attempt(1);
+        outPut();
+    }
 }
---
+++ src/tlgk_branch_and_bound/KnapsackProblem01.java
@@ -0,0 +1,89 @@
+package tlgk_branch_and_bound;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class KnapsackProblem01 {
+
+    static int n;
+    static double M;
+    static double[] W = new double[100];    //weight
+    static double[] V = new double[100]; //value
+
+    static int[] a = new int[100];
+    static int[] x = new int[100];
+
+    static double weight, value, vmax;
+
+    static void init() {
+        try {
+            Scanner scanner = new Scanner(new File("C:\\Users\\ANHTDAM\\IdeaProjects\\algorithms-design-analysis\\src\\input.txt"));
+            while (scanner.hasNext()) {
+                n = scanner.nextInt();
+                M = scanner.nextInt();
+                for (int i = 1; i <= n; i++) {
+                    V[i] = scanner.nextInt();
+                }
+                for (int i = 1; i <= n; i++) {
+                    W[i] = scanner.nextInt();
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        vmax = -1;
+        weight = 0;
+        value = 0;
+    }
+
+    static void update() {
+        if (weight <= M) {
+            if (value > vmax) {
+                vmax = value;
+                for (int i = 1; i <= n; i++) {
+                    x[i] = a[i];
+                }
+            }
+        }
+    }
+
+    static void trySolution(int i) {
+        for (int j = 0; j <= 1; j++) {
+            a[i] = j;
+            if (j == 1) {
+                weight = weight + W[i];
+                value = value + V[i];
+            }
+            if (weight <= M) {
+                if (i == n) {
+                    update();
+                } else {
+                    trySolution(i + 1);
+                }
+            }
+            if (j == 1) {
+                weight = weight - W[i];
+                value = value - V[i];
+            }
+        }
+    }
+
+    static void printResult(){
+        System.out.println("Tong gia tri lon nhat: " + vmax);
+        System.out.println("Chon cac do vat sau: ");
+        for (int i = 1; i <= n; i++) {
+            if(x[i] == 1){
+                System.out.print(i + "\t");
+            }
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        init();
+        trySolution(1);
+        printResult();
+    }
+}
